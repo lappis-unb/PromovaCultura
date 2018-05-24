ba741b279f8f07e2bd1a4cfae2a6cbbbed05f164
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-05-24 11:07:06 -0300

    Adding funil component

--- promova-cultura/src/router/index.js
+++ promova-cultura/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import BrazilMap from '@/pages/BrazilMap'
 import BrazilMapRegion from '@/pages/BrazilMapRegion'
+import Funil from '@/pages/Funil'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/region',
       name: 'BrazilMapRegion',
       component: BrazilMapRegion
+    },
+    {
+      path: '/funil',
+      name: 'Funil',
+      component: Funil
     }
   ]
 })
