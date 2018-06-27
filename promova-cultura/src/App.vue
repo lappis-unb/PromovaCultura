e8c7fcf7cf5a3b9889c3926cbfceab2a8cd84326
eduardonunes <eduardonunes2525@gmail.com>
2018-06-27 13:40:20 -0300

    Adding slick carousel in home page

--- promova-cultura/src/App.vue
+++ promova-cultura/src/App.vue
@@ -10,25 +10,24 @@
 </template>
 
 <script>
-import NavbarMenu from '@/components/NavbarMenu'
+import NavbarMenu from "@/components/NavbarMenu";
 
-import '../static/bootstrap/bootstrap.min.js'
-import "../static/jsmaps/jsmaps.js"
-import "../static/jsmaps/jsmaps-panzoom.js"
-import "../static/jsmaps/jsmaps-libs.js"
-import "../static/jsmaps/lodash.js"
-
+import "../static/bootstrap/bootstrap.min.js";
+import "../static/jsmaps/jsmaps.js";
+import "../static/jsmaps/jsmaps-panzoom.js";
+import "../static/jsmaps/jsmaps-libs.js";
+import "../static/jsmaps/lodash.js";
 
 export default {
-    name: 'App',
-    components: {
-      'navbar-menu': NavbarMenu
-    }
-}
+  name: "App",
+  components: {
+    "navbar-menu": NavbarMenu
+  }
+};
 </script>
 
 <style>
-  .main-container {
-    padding-top: 50px;
-  }
+.main-container {
+  padding-top: 50px;
+}
 </style>
