75282c8f4b1d2dc0c43302f1a1ecfae1b6568ce5
Fabio Teixeira <fabio1079@gmail.com>
2018-07-04 11:32:54 -0300

    Fetch projetos, incentivadores and proponentes in a single query
    Co-authored-by: João Guilherme <joaaogui@gmail.com>

--- promova-cultura/src/util/apiComunication.js
+++ promova-cultura/src/util/apiComunication.js
@@ -1,109 +1,53 @@
 import cacheFetch from 'cache-fetch';
 
 
-function fetchData(type = "", projectSegment = "") {
+export const fetchData = (types, projectSegment = "") => {
     let segmentQuery = "";
 
     if (projectSegment !== "" && typeof projectSegment === "string") {
         segmentQuery = `(segmento: "${projectSegment}")`;
     }
 
-    const query = `
-        query {
-            ${type} ${segmentQuery} {
-                local,
-                quantidade
-            }
-        }
+
+    let query = `
+      query {
     `;
 
+    query += types.map(type => `
+        ${type} ${segmentQuery} {
+            local,
+            quantidade
+        }
+    `).join("");
+
+    query += "}";
+
     const queryAsUrl = encodeURI(query);
+
     const promise = cacheFetch(`https://salic.dev.lappis.rocks/graphql?query=${queryAsUrl}`);
 
     return promise;
 }
 
-function getDataFromKey(key) {
-    return promise =>
-        promise.then(res => res.json()).then(json => {
-            const listOfUfs = {};
+export const batchFetch = async (local, segment="") => {
+    let types = [
+        `projetos_por_${local}`,
+        `proponentes_por_${local}`,
+        `incentivadores_por_${local}`,
+    ];
 
-            for (let region of json.data[key]) {
-                listOfUfs[region.local] = region.quantidade;
-            }
+    let response = await fetchData(types, segment);
+    let { data } = await response.json();
+    let listOfUfs = {};
 
-            return Promise.resolve(listOfUfs);
-        });
-}
+    types.forEach(type => {
+        listOfUfs[type] = {};
 
-// Updates projects with the response from a graphql query
-function fillProjects(resourceName, promise, projects) {
-    const projetos_por_uf = getDataFromKey(resourceName)(promise);
-
-    projetos_por_uf.then(listOfUfs => {
-        projects.projects = listOfUfs;
+        for (let region of data[type]) {
+            listOfUfs[type][region.local] = region.quantidade;
+        }
     });
+
+    return listOfUfs;
 }
 
-function fillProponentes(resourceName, promise, proponentes) {
-    const proponentes_por_uf = getDataFromKey(resourceName)(
-        promise
-    );
-
-    proponentes_por_uf.then(listOfUfs => {
-        proponentes.proponentes = listOfUfs;
-    });
-}
-
-function fillIncentivadores(resourceName, promise, incentivadores) {
-    const incentivadores_por_uf = getDataFromKey(
-        resourceName
-    )(promise);
-
-    incentivadores_por_uf.then(listOfUfs => {
-        incentivadores.incentivadores = listOfUfs;
-    });
-}
-
-function fetchResource(resourceName, out, segment) {
-    switch (resourceName.substring(0, 7)) {
-        case "projeto":
-            fetchProjects(resourceName, out, segment);
-            break;
-        case "propone":
-            fetchProponents(resourceName, out, segment);
-            break;
-        case "incenti":
-            fetchIncentivadores(resourceName, out, segment);
-            break;
-    }
-}
-
-function fetchProjects(resourceName, projects, segment) {
-    const promise = fetchWithSegment(resourceName, segment);
-    fillProjects(resourceName, promise, projects);
-}
-
-function fetchProponents(resourceName, proponentes, segment) {
-    const promise = fetchWithSegment(resourceName, segment);
-    fillProponentes(resourceName, promise, proponentes);
-}
-
-function fetchIncentivadores(resourceName, incentivadores, segment) {
-    const promise = fetchWithSegment(resourceName, segment);
-    fillIncentivadores(resourceName, promise, incentivadores);
-}
-
-function fetchWithSegment(type, segment) {
-    let promise = null;
-
-    if (segment === "Todos os segmentos") {
-        promise = fetchData(type);
-    } else {
-        promise = fetchData(type, segment);
-    }
-
-    return promise;
-}
-
-export default fetchResource;
