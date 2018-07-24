b8ef902bfac3197e1dc02d85a742cda9fe0c8ac5
Victor Navarro <victor.matias.navarro@gmail.com>
2018-07-24 12:34:03 -0300

    Starting to turn card into a component

---
+++ promova-cultura-vuejs/src/components/funilPrototype/funilPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <funil-card :people="people"/>
+</template>
+
+<script>
+import FunilCard from "@/components/funilPrototype/funilCard"
+
+export default {
+  name: "FunilPage",
+  components: {
+    "funil-card": FunilCard,
+  },
+  props: {
+  },
+  data() {
+    return {
+        people: {
+            proponentes: 10000000,
+            incentivadores: 1000000
+        }
+    }
+  }
+};
+</script>
