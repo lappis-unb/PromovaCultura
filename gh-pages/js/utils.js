f5a1c308dd1729f36b1a1dfc1fdb88c10deb38b7
João Guilherme <joaaogui@gmail.com>
2018-08-15 15:26:14 -0300

    Fixes tooltips and legend
    Co-authored-by: Guilherme Lacerda <guiglacerda@gmail.com>

--- gh-pages/js/utils.js
+++ gh-pages/js/utils.js
@@ -1,18 +1,3 @@
-function dropSelect(e) {
-  let segment = e.value
-  console.log()
-  data.forEach(function(element) {
-    element.symbolSize = Math.floor(Math.random() * Math.floor(60));
-  })
-
-  links.forEach(function(element) {
-    element.lineStyle.width = Math.floor(Math.random() * Math.floor(15));
-  })
-  console.log(links)
-
-  myChart.setOption(option);
-}
-
 categories = [{
   'name': 'Norte',
   'itemStyle': {
@@ -70,309 +55,195 @@ ufDict = {
   "Mato Grosso do Sul": "MS",
 }
 
-ufList = [
-  "Acre",
-  "Alagoas",
-  "Amazonas",
-  "Amapá",
-  "Bahia",
-  "Ceará",
-  "Distrito Federal",
-  "Espírito Santo",
-  "Goiás",
-  "Maranhão",
-  "Mato Grosso",
-  "Mato Grosso do Sul",
-  "Minas Gerais",
-  "Pará",
-  "Paraíba",
-  "Paraná",
-  "Pernambuco",
-  "Piauí",
-  "Rio de Janeiro",
-  "Rio Grande do Norte",
-  "Rondônia",
-  "Rio Grande do Sul",
-  "Roraima",
-  "Santa Catarina",
-  "Sergipe",
-  "São Paulo",
-  "Tocantins"
-]
+var _links = []
 
 chartData = [{
   "name": "Acre",
-  "x": 0,
-  "y": 0,
   "category": "Norte",
   "symbolSize": 15,
-  "draggable": "true",
-  "value": 27
+  "value": 27,
+  "x": 3,
+  "y": -2,
 }, {
   "name": "Amapá",
-  "value": 8,
-  "symbolSize": 30,
   "category": "Norte",
-  "draggable": "true",
-  "x": 0,
-  "y": 0,
+  "symbolSize": 30,
+  "value": 8,
+  "x": 1,
+  "y": -6,
 }, {
   "name": "Amazonas",
-  "symbolSize": 16,
   "category": "Norte",
-  "draggable": "true",
+  "symbolSize": 16,
   "value": 7,
   "x": 0,
-  "y": 0,
+  "y": -1,
 }, {
   "name": "Pará",
-  "symbolSize": 60,
   "category": "Norte",
-  "draggable": "true",
+  "symbolSize": 60,
   "value": 7,
-  "x": 0,
-  "y": 0,
+  "x": 7,
+  "y": 1,
 }, {
   "name": "Rondônia",
-  "symbolSize": 15,
   "category": "Norte",
-  "draggable": "true",
+  "symbolSize": 15,
   "value": 7,
-  "x": 0,
-  "y": 0,
+  "x": 4,
+  "y": -6,
 }, {
   "name": "Roraima",
-  "value": 60,
-  "symbolSize": 60,
   "category": "Norte",
-  "draggable": "true",
-  "x": 0,
-  "y": 0,
+  "symbolSize": 60,
+  "value": 60,
+  "x": 5,
+  "y": -3,
 }, {
   "name": "Tocantins",
   "symbolSize": 50,
   "category": "Norte",
-  "draggable": "true",
   "value": 7,
-  "x": 0,
-  "y": 0,
+  "x": 6,
+  "y": 4,
 }, {
   "name": "Alagoas",
+  "category": "Nordeste",
   "value": 20,
   "symbolSize": 30,
-  "category": "Nordeste",
-  "draggable": "true",
-  "x": 0,
-  "y": 0,
+  "x": 1,
+  "y": 7,
 }, {
   "name": "Bahia",
-  "symbolSize": 16,
   "category": "Nordeste",
-  "draggable": "true",
+  "symbolSize": 16,
   "value": 7,
-  "x": 0,
-  "y": 0,
+  "x": 1,
+  "y": -3,
 }, {
   "name": "Ceará",
-  "symbolSize": 16,
   "category": "Nordeste",
-  "draggable": "true",
+  "symbolSize": 16,
   "value": 7,
-  "x": 0,
-  "y": 0,
+  "x": -6,
+  "y": -4,
 }, {
   "name": "Maranhão",
-  "value": 6,
-  "symbolSize": 18,
   "category": "Nordeste",
-  "draggable": "true",
-  "x": 0,
-  "y": 0,
+  "symbolSize": 18,
+  "value": 6,
+  "x": 4,
+  "y": 6,
 }, {
   "name": "Paraíba",
-  "symbolSize": 15,
   "category": "Nordeste",
-  "draggable": "true",
+  "symbolSize": 15,
   "value": 7,
-  "x": 0,
-  "y": 0,
+  "x": -4,
+  "y": 3,
 }, {
   "name": "Pernambuco",
-  "symbolSize": 16,
   "category": "Nordeste",
-  "draggable": "true",
+  "symbolSize": 16,
   "value": 7,
-  "x": 0,
-  "y": 0,
+  "x": -1,
+  "y": 2,
 }, {
   "name": "Piauí",
-  "value": 6,
-  "symbolSize": 18,
   "category": "Nordeste",
-  "draggable": "true",
-  "x": 0,
-  "y": 0,
+  "symbolSize": 18,
+  "value": 6,
+  "x": 2,
+  "y": -5,
 }, {
   "name": "Rio Grande do Norte",
-  "symbolSize": 16,
   "category": "Nordeste",
-  "draggable": "true",
+  "symbolSize": 16,
   "value": 7,
-  "x": 0,
-  "y": 0,
+  "x": 7,
+  "y": -1,
 }, {
   "name": "Sergipe",
   "symbolSize": 16,
   "category": "Nordeste",
-  "draggable": "true",
   "value": 7,
-  "x": 0,
-  "y": 0,
+  "x": -5,
+  "y": 7,
 }, {
   "name": "Distrito Federal",
   "value": 5,
   "symbolSize": 40,
   "category": "Centro-Oeste",
-  "draggable": "true",
-  "x": 0,
-  "y": 0,
+  "x": 2,
+  "y": 5,
 }, {
   "name": "Goiás",
-  "symbolSize": 30,
   "category": "Centro-Oeste",
-  "draggable": "true",
+  "symbolSize": 30,
   "value": 7,
-  "x": 0,
+  "x": 4,
   "y": 0,
 }, {
   "name": "Mato Grosso",
-  "symbolSize": 7,
   "category": "Centro-Oeste",
-  "draggable": "true",
+  "symbolSize": 7,
   "value": 7,
-  "x": 0,
-  "y": 0,
+  "x": -1,
+  "y": 5,
 }, {
   "name": "Mato Grosso do Sul",
-  "value": 4,
-  "symbolSize": 18,
   "category": "Centro-Oeste",
-  "draggable": "true",
-  "x": 0,
-  "y": 0,
+  "symbolSize": 18,
+  "value": 4,
+  "x": -3,
+  "y": 1,
 }, {
   "name": "Espírito Santo",
-  "symbolSize": 20,
   "category": "Sudeste",
-  "draggable": "true",
+  "symbolSize": 20,
   "value": 7,
-  "x": 0,
-  "y": 0,
+  "x": -3,
+  "y": -6,
 }, {
   "name": "Minas Gerais",
-  "symbolSize": 25,
   "category": "Sudeste",
-  "draggable": "true",
+  "symbolSize": 25,
   "value": 7,
-  "x": 0,
+  "x": -7,
   "y": 0,
 }, {
   "name": "Rio de Janeiro",
-  "value": 7,
-  "symbolSize": 10,
   "category": "Sudeste",
-  "draggable": "true",
-  "x": 0,
-  "y": 0,
+  "symbolSize": 10,
+  "value": 7,
+  "x": 7,
+  "y": 7,
 }, {
   "name": "São Paulo",
-  "symbolSize": 75,
   "category": "Sudeste",
-  "draggable": "true",
+  "symbolSize": 75,
   "value": 7,
-  "x": 0,
-  "y": 0,
+  "x": 2,
+  "y": 2,
 }, {
   "name": "Paraná",
-  "value": 7,
-  "symbolSize": 30,
   "category": "Sul",
-  "draggable": "true",
-  "x": 0,
-  "y": 0,
+  "symbolSize": 30,
+  "value": 7,
+  "x": -3,
+  "y": -3,
 }, {
   "name": "Rio Grande do Sul",
-  "symbolSize": 7,
   "category": "Sul",
-  "draggable": "true",
+  "symbolSize": 7,
   "value": 7,
-  "x": 0,
-  "y": 0,
+  "x": -7,
+  "y": 6,
 }, {
   "name": "Santa Catarina",
-  "value": 30,
-  "symbolSize": 30,
   "category": "Sul",
-  "draggable": "true",
-  "x": 0,
-  "y": 0,
+  "symbolSize": 30,
+  "value": 30,
+  "x": 7,
+  "y": -5,
 }]
-
-_links = [{}]
-
-// var iti = {};
-//
-// function omelete() {
-//   var ligations = {
-//     "data": {
-//       "deslocamentos": [{
-//           "UFOrigem": "Minas Gerais",
-//           "UFDestino": "São Paulo"
-//         },
-//         {
-//           "UFOrigem": "Minas Gerais",
-//           "UFDestino": "Minas Gerais"
-//         },
-//         {
-//           "UFOrigem": "Rio de Janeiro",
-//           "UFDestino": "Minas Gerais"
-//         },
-//         {
-//           "UFOrigem": "Rio de Janeiro",
-//           "UFDestino": "Rio Grande do Sul"
-//         },
-//         {
-//           "UFOrigem": "Rio de Janeiro",
-//           "UFDestino": "Distrito Federal"
-//         },
-//         {
-//           "UFOrigem": "São Paulo",
-//           "UFDestino": "Minas Gerais"
-//         },
-//         {
-//           "UFOrigem": "São Paulo",
-//           "UFDestino": "Minas Gerais"
-//         },
-//         {
-//           "UFOrigem": "Minas Gerais",
-//           "UFDestino": "São Paulo"
-//         },
-//       ]
-//     }
-//   }
-//
-//   $.each(ligations.data.deslocamentos, function(index, value) {
-//     var result = value["UFOrigem"] + " - " + value["UFDestino"];
-//     var inverse = value["UFDestino"] + " - " + value["UFOrigem"];
-//
-//     if (iti[result] !== undefined) {
-//       iti[result] += 1;
-//     } else {
-//       if (iti[inverse] !== undefined) {
-//         iti[inverse] += 1;
-//       } else {
-//         iti[result] = 1;
-//       }
-//     }
-//   });
-// }
