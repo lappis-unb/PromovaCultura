d087a05e867528c89627c4fe35afdc1289569c67
João Guilherme <joaaogui@gmail.com>
2018-09-05 17:42:42 -0300

    Update apiComunication.js

--- promova-cultura-vuejs/src/util/apiComunication.js
+++ promova-cultura-vuejs/src/util/apiComunication.js
@@ -14,8 +14,7 @@ export const fetchData = (types, projectSegment = "") => {
 
     const queryAsUrl = encodeURI(query);
 
-    // const promise = cacheFetch(`https://salic.dev.lappis.rocks/graphql?query=${queryAsUrl}`);
-    const promise = cacheFetch(`http://localhost:5000/graphql?query=${queryAsUrl}`);
+    const promise = cacheFetch(`https://salic.dev.lappis.rocks/graphql?query=${queryAsUrl}`);
 
     return promise;
 }
