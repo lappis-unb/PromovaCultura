c950b2410e8d9bcdf0b4dbf240147ecf813d9071
Marcos Ronaldo <marcos.rpj2@gmail.com>
2018-09-17 17:01:49 -0300

    Create and apply standard navbar to all prototypes
    Co-authored-by: Ania Gomes <aniaog95@gmail.com>
    Co-authored-by: Marcos Ronaldo <marcos.rpj2@gmail.com>

--- promova-cultura-vuejs/src/components/NavbarMenu.vue
+++ promova-cultura-vuejs/src/components/NavbarMenu.vue
@@ -3,12 +3,12 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,700,800|Poppins:300,400,500,600,700" rel="stylesheet">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-          <!--<router-link class="navbar-brand" to="/">{{title}}</router-link>-->
-          <a href="https://lappis-unb.github.io/PromovaCultura/">Promova Cultura</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-              aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-          </button>
+        <!--<router-link class="navbar-brand" to="/">{{title}}</router-link>-->
+        <a href="https://lappis-unb.github.io/PromovaCultura/">Promova Cultura</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
       </div>
     </nav>
   </header>
@@ -40,6 +40,7 @@ h1 {
 }
 a {
   color: #333;
+  font-size: 20px; 
 }
 a:hover {
   opacity: 0.8;
@@ -54,5 +55,9 @@ a:hover {
 
 .navbar-nav {
   font-size: 15px;
+}
+
+.container{
+  padding: 6px 0 5px 0;
 }
 </style>
