1bc292a5989f8e9ac34c473db2589b8555cefa14
João Guilherme <joaaogui@gmail.com>
2018-09-17 14:51:00 -0300

    Adding icon to filter nav
    Co-authored-by: Guilherme Lacerda <guiglacerda@gmail.com>

--- promova-cultura-vuejs/src/components/Map/filters/CollapsedFilter.vue
+++ promova-cultura-vuejs/src/components/Map/filters/CollapsedFilter.vue
@@ -1,55 +1,56 @@
 <template>
-    <div id="accordion">
-        <div class="card">
-            <div class="card-header" id="headingOne">
-                <h5 class="mb-0">
-                    <a href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    Filtrar
-                    </a>
-                </h5>
-            </div>
-            <div id="collapseOne" class="collapse hide" aria-labelledby="headingOne" data-parent="#accordion">
-                <div class="card-body">
-                    <filters
-                        @updatedSegment="updateSegment"
-                        @showProponentes="changeValueProponente"
-                        @showIncentivadores="changeValueIncentivadores"
-                        @changeLevel="changeLevel"
-                    />
-                </div>
-            </div>
-        </div>
+<div class="card" id="accordion">
+  <legend class="card-header">
+    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+      <i class="fa" aria-hidden="true"></i>
+        Filtrar
+      </button>
+  </legend>
+  <div id="collapseOne" class="collapse hide" aria-labelledby="headingOne" data-parent="#accordion">
+    <div class="card-body">
+      <filters @updatedSegment="updateSegment" @showProponentes="changeValueProponente" @showIncentivadores="changeValueIncentivadores" @changeLevel="changeLevel" />
     </div>
+  </div>
+</div>
 </template>
 
 <script>
 import Filters from "@/components/Map/filters/Filters"
 
 export default {
-    components: {
-        "filters": Filters,
+  components: {
+    "filters": Filters,
+  },
+  methods: {
+    updateSegment: function(selected) {
+      this.$emit('updatedSegment', selected)
     },
-    methods: {
-        updateSegment: function(selected){
-            this.$emit('updatedSegment', selected)
-        },
-        changeValueProponente: function(proponentesIsActivated){
-            this.$emit('showProponentes', proponentesIsActivated)
-        },
-        changeValueIncentivadores: function(incentivadoresIsActivated){
-            this.$emit('showIncentivadores', incentivadoresIsActivated)
-        },
-        changeLevel: function(level){
-            this.$emit('changeLevel', level);
-        }
+    changeValueProponente: function(proponentesIsActivated) {
+      this.$emit('showProponentes', proponentesIsActivated)
+    },
+    changeValueIncentivadores: function(incentivadoresIsActivated) {
+      this.$emit('showIncentivadores', incentivadoresIsActivated)
+    },
+    changeLevel: function(level) {
+      this.$emit('changeLevel', level);
     }
+  }
 }
 </script>
 <style scoped>
-    .card-header a{
-        color: #111;
-        font-size: 16px;
-        font-weight: bold;
-        text-transform: uppercase;
-    }
+
+[data-toggle="collapse"] .fa:before {
+  content: "\f139";
+}
+
+[data-toggle="collapse"].collapsed .fa:before {
+  content: "\f13a";
+}
+
+.card-header a {
+  color: #111;
+  font-size: 16px;
+  font-weight: bold;
+  text-transform: uppercase;
+}
 </style>
