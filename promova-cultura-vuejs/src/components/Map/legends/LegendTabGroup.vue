cb66a71fb6166a06047eb8ef0ed35d1daf55d8b4
Marcos Ronaldo <marcos.rpj2@gmail.com>
2018-09-19 14:30:10 -0300

    Fix tab legend

--- promova-cultura-vuejs/src/components/Map/legends/LegendTabGroup.vue
+++ promova-cultura-vuejs/src/components/Map/legends/LegendTabGroup.vue
@@ -5,16 +5,16 @@
             <a class="nav-link active" id="projeto-tab" data-toggle="tab" href="#projeto" role="tab" aria-controls="projeto" aria-selected="true">Projetos</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" v-bind:class="{ disabled: !filtersActivate.proponentes}" id="proponente-tab" data-toggle="tab" href="#proponente" role="tab" aria-controls="proponente" aria-selected="false">Proponentes</a>
+            <a class="nav-link" id="proponente-tab" data-toggle="tab" href="#proponente" role="tab" aria-controls="proponente" aria-selected="false">Proponentes</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" v-bind:class="{ disabled: !filtersActivate.incentivadores}" id="incentivador-tab" data-toggle="tab" href="#incentivador" role="tab" aria-controls="incentivador" aria-selected="false">Incentivadores</a>
+            <a class="nav-link" id="incentivador-tab" data-toggle="tab" href="#incentivador" role="tab" aria-controls="incentivador" aria-selected="false">Incentivadores</a>
         </li>
     </ul>
     <div class="tab-content legend-content" id="myTabContent">
-        <div class="tab-pane fade show active" id="projeto" aria-labelledby="projeto-tab" role="tabpanel"><base-legend :dataLegend="legends.heatMap" :typeOfData="'projetos'" /></div>
-        <div class="tab-pane fade" v-if="filtersActivate.proponentes" id="proponente" aria-labelledby="proponente-tab" role="tabpanel"><base-legend :dataLegend="legends.proponentes" :typeOfData="'proponentes'" /></div>
-        <div class="tab-pane fade" v-if="filtersActivate.incentivadores" id="incentivador" aria-labelledby="incentivador-tab" role="tabpanel"><base-legend :dataLegend="legends.incentivadores" :typeOfData="'incentivadores'" /></div>
+        <div class="tab-pane fade show active" id="projeto" aria-labelledby="projeto-tab" role="tabpanel"><base-legend :dataLegend="legends.heatMap" :typeOfData="'projetos'" :filtersActivate="filtersActivate" /></div>
+        <div class="tab-pane fade" id="proponente" aria-labelledby="proponente-tab" role="tabpanel"><base-legend :dataLegend="legends.proponentes" :typeOfData="'proponentes'" :filtersActivate="filtersActivate"/></div>
+        <div class="tab-pane fade" id="incentivador" aria-labelledby="incentivador-tab" role="tabpanel"><base-legend :dataLegend="legends.incentivadores" :typeOfData="'incentivadores'" :filtersActivate="filtersActivate"/></div>
     </div>
 </div>
 </template>
