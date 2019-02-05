e72b26a0097b15efd137b9e1bd2bfc152a2d5a34
pablodiegoss <pablodiegoss@hotmail.com>
2019-02-05 09:46:24 -0200

    Add HeatList to mobile

--- promova-cultura-vuejs/src/components/Map/info/ProponentMobileInfo.vue
+++ promova-cultura-vuejs/src/components/Map/info/ProponentMobileInfo.vue
@@ -3,8 +3,7 @@
     <h1 class="proponent-title">Captação de recurso por UF desde 1992</h1>
 
     <div id="main-content">
-      <div id="loading-placeholder">
-      </div>
+      <div id="loading-placeholder"></div>
       <div class="sticky-top" style="overflow: hidden; background-color: white">
         <div class="row">
           <div class="offset-1 col-5 sticky">Estado</div>
@@ -12,20 +11,26 @@
         </div>
       </div>
       <div v-for="(uf, index) in Object.keys(this.data.proponents)">
-        <div class="row collapsibleState collapsed uf-item" data-toggle="collapse" :data-target="'#collapseStateContent' + index">
-          <div class="col-6 state" v-if="uf !== '  '">{{ufs[uf]}}</div>
-          <div class="col-6 state" v-else>Sem Estado</div>
+        <div
+          v-if="uf !== '  '"
+          :id="uf"
+          class="row collapsibleState collapsed uf-item"
+          data-toggle="collapse"
+          :data-target="'#collapseStateContent' + index"
+        >
+          <div class="col-6 state">{{ufs[uf]}}</div>
+          <!-- <div class="col-6 state" v-else>Sem Estado</div> -->
           <div class="col-4 amount">
-            <span v-if="data.raisedAmount[uf] < 1000000000 && data.raisedAmount[uf] >= 1000000">
-              R$ {{parseFloat((data.raisedAmount[uf]/1000000)).toFixed(1)}} mi
-            </span>
-            <span v-else-if="(data.raisedAmount[uf] >= 1000000000)">
-              R$ {{parseFloat((data.raisedAmount[uf]/1000000000).toFixed(1)).toLocaleString("pt-BR")}} bi
-            </span>
+            <span
+              v-if="data.raisedAmount[uf] < 1000000000 && data.raisedAmount[uf] >= 1000000"
+            >R$ {{parseFloat((data.raisedAmount[uf]/1000000)).toFixed(1)}} mi</span>
+            <span
+              v-else-if="(data.raisedAmount[uf] >= 1000000000)"
+            >R$ {{parseFloat((data.raisedAmount[uf]/1000000000).toFixed(1)).toLocaleString("pt-BR")}} bi</span>
             <span v-else>R$ {{parseFloat(data.raisedAmount[uf]).toLocaleString("pt-BR")}}</span>
           </div>
           <div class="white-space-bar"></div>
-          <div class="col-2 arrow">
+          <div :id="uf+'-arrow'" class="col-2 arrow">
             <span class="icon-filter">
               <i class="fa" aria-hidden="true"></i>
             </span>
@@ -39,17 +44,21 @@
           </div>
           <div class="row">
             <div class="col-6">Valor aprovado</div>
-            <div class="col-6">{{parseFloat(data.approvedAmount[uf]).toLocaleString("pt-BR", {
+            <div class="col-6">
+              {{parseFloat(data.approvedAmount[uf]).toLocaleString("pt-BR", {
               style: "currency",
               currency: "BRL"
-            })}}</div>
+              })}}
+            </div>
           </div>
           <div class="row">
             <div class="col-6">Valor captado</div>
-            <div class="col-6">{{parseFloat(data.raisedAmount[uf]).toLocaleString("pt-BR", {
+            <div class="col-6">
+              {{parseFloat(data.raisedAmount[uf]).toLocaleString("pt-BR", {
               style: "currency",
               currency: "BRL"
-            })}}</div>
+              })}}
+            </div>
           </div>
         </div>
       </div>
@@ -66,181 +75,187 @@
 </template>
 
 <script>
-  import uf from "@/util/ufs.js";
-  import $ from "jquery";
-  import LoadingOverlay from "gasparesganga-jquery-loading-overlay";
+import uf from "@/util/ufs.js";
+import mobileActions from "@/util/mobileMapActions.js";
+import $ from "jquery";
+import MapPage from "@/pages/MapPage";
+import LoadingOverlay from "gasparesganga-jquery-loading-overlay";
 
-  export default {
-    props: {
-      data: Object,
-    },
-    watch: {
-      data: {
-        handler(data) {
-          this.updateTotals();
-        },
-        deep: true
+export default {
+  props: {
+    data: Object
+  },
+  watch: {
+    data: {
+      handler(data) {
+        this.updateTotals();
       },
-    },
-    mounted() {
-      $("#loading-placeholder").LoadingOverlay("show", {
-        background: "rgba(255, 255, 255, 1)",
-        image: "",
-        fontawesome: "fa fa-circle-notch fa-spin",
-        fontawesomeColor: "#565656",
-        imageClass: "loading-animation"
+      deep: true
+    }
+  },
+  data() {
+    return {
+      ufs: uf,
+      totalProponents: 0,
+      totalapprovedAmount: 0,
+      totalRaisedAmount: 0
+    };
+  },
+  methods: {
+    updateTotals() {
+      this.totalProponents = this.data.totals["proponents"];
+      this.totalapprovedAmount = this.data.totals[
+        "approvedAmount"
+      ].toLocaleString("pt-BR", {
+        minimumFractionDigits: 2
       });
-    },
-    data() {
-      return {
-        ufs: uf,
-        totalProponents: 0,
-        totalapprovedAmount: 0,
-        totalRaisedAmount: 0
-      }
-    },
-    methods: {
-      updateTotals() {
-        this.totalProponents = this.data.totals["proponents"]
-        this.totalapprovedAmount = (this.data.totals["approvedAmount"]).toLocaleString('pt-BR', {
+      this.totalRaisedAmount = this.data.totals["raisedAmount"].toLocaleString(
+        "pt-BR",
+        {
           minimumFractionDigits: 2
-        })
-        this.totalRaisedAmount = (this.data.totals["raisedAmount"]).toLocaleString('pt-BR', {
-          minimumFractionDigits: 2
-        })
-        $("#loading-placeholder").LoadingOverlay("hide");
-        $(".uf-item").toggleClass("visible");
-        $(".sticky-top").toggleClass("visible");
-      }
+        }
+      );
+
+      let maxValue = this.getMaxValue(this.data.raisedAmount, this.ufs);
+      let legendsHeatMap = MapPage.methods.getMapLegend(maxValue);
+      console.log(legendsHeatMap);
+
+      mobileActions.makeHeatList(
+        this.ufs,
+        this.data.raisedAmount,
+        legendsHeatMap
+      );
     },
+    getMaxValue(values) {
+      let maxValue = 0;
+      let ufValues = Object.values(values);
+      for (let i = 0; i < Object.keys(this.ufs).length; i++) {
+        if (ufValues[i] > maxValue) {
+          maxValue = ufValues[i];
+        }
+      }
+      // +1 to include the max value state inside the range.
+      return maxValue + 1;
+    }
   }
+};
 </script>
 
 <style scoped>
-  H1 {
-    text-align: center;
-  }
+h1 {
+  text-align: center;
+}
 
-  #main-content {
-    position: relative;
-    width: 100%;
-    margin-bottom: 38px;
-  }
+#main-content {
+  position: relative;
+  width: 100%;
+  margin-bottom: 38px;
+}
 
-  #loading-placeholder {
-    width: 100%;
-    position: absolute;
-    top: 30px;
-    left: 0;
-    height: 200px;
-  }
+#loading-placeholder {
+  width: 100%;
+  position: absolute;
+  top: 30px;
+  left: 0;
+  height: 200px;
+}
+.sticky {
+  font-size: 20px;
+  color: #808080;
+}
 
-  .sticky-top, .uf-item {
-    visibility: hidden;
-  }
+.fa {
+  font-size: 30px;
+  width: 50%;
+  margin: auto;
+}
 
-  .visible {
-    visibility: visible;
-  }
+.collapsibleState {
+  background-color: #ccc;
+  position: relative;
+  border-radius: 8px;
+  margin: 8px;
+  border-left: solid 8px #ffffff;
+}
 
-  .sticky {
-    font-size: 20px;
-    color: #808080;
-  }
+.state,
+.amount {
+  padding: 12px 8px;
+  background-color: #e6e6e6;
+}
 
-  .fa {
-    font-size: 30px;
-    width: 50%;
-    margin: auto;
-  }
+.state {
+  font-weight: bold;
+}
 
-  .collapsibleState {
-    background-color: #CCC;
-    position: relative;
-    border-radius: 8px;
-    margin: 8px;
-    border-left: solid 8px #3aa53a;
-  }
+.amount {
+  text-align: right;
+}
 
-  .state, .amount {
-    padding: 12px 8px;
-    background-color: #e6e6e6;
-  }
+.arrow {
+  position: absolute;
+  background-color: #3aa53a;
+  height: 100%;
+  top: 0;
+  padding: 0 0 0 5px;
+  right: 0;
+  width: 50%;
+  margin: auto;
+  border-radius: 0 8px 8px 0;
+  color: #eee;
+  text-align: center;
+  line-height: 60px;
+  vertical-align: middle;
+}
 
-  .state {
-    font-weight: bold;
-  }
+.arrow span {
+  margin: auto;
+  position: relative;
+}
 
-  .amount {
-    text-align: right;
-  }
+.collapsibleStateContent {
+  background-color: #ccc;
+  padding: 16px;
+  margin: 4px 16px 16px;
+  border-radius: 8px;
+}
 
-  .arrow {
-    position: absolute;
-    background-color: #3aa53a;
-    height: 100%;
-    top: 0;
-    padding: 0 0 0 5px;
-    right: 0;
-    width: 50%;
-    margin: auto;
-    border-radius: 0 8px 8px 0;
-    color: #eee;
-    text-align: center;
-    line-height: 60px;
-    vertical-align: middle
-  }
+[data-toggle="collapse"] .fa:before {
+  content: "\f078";
+}
 
-  .arrow span {
-    margin: auto;
-    position: relative;
-  }
+[data-toggle="collapse"].collapsed .fa:before {
+  content: "\f054";
+  padding: 0;
+}
 
-  .collapsibleStateContent {
-    background-color: #CCC;
-    padding: 16px;
-    margin: 4px 16px 16px;
-    border-radius: 8px;
+.proponent-title {
+  font-size: 28px;
+  margin-bottom: 30px;
+  font-weight: bold;
+}
 
-  }
+.footer-fixed {
+  background-color: #ccc;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  z-index: 2;
+}
 
-  [data-toggle="collapse"] .fa:before {
-    content: "\f078";
-  }
+.sticky-total {
+  color: #333;
+  font-size: 20px;
+}
 
-  [data-toggle="collapse"].collapsed .fa:before {
-    content: "\f054";
-    padding: 0;
-  }
+.sticky-total span {
+  font-weight: bold;
+}
 
-  .proponent-title {
-    font-size: 28px;
-    margin-bottom: 30px;
-    font-weight: bold;
-
-  }
-
-  .footer-fixed {
-    background-color: #ccc;
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    z-index: 2;
-  }
-
-  .sticky-total {
-    color: #333;
-    font-size: 20px;
-  }
-
-  .sticky-total span {
-    font-weight: bold;
-  }
-
-  .white-space-bar {
-    width: 5px;
-    background-color: white;
-    z-index: 1;
-  }
+.white-space-bar {
+  width: 5px;
+  background-color: white;
+  z-index: 1;
+}
 </style>
